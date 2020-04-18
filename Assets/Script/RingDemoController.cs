f19c64ae2ad416e8ae1ad3eb57ff242504a06018
anson1788 <anson1788@gmail.com>
2020-04-19 02:59:36 +0800

    update unity version

--- Assets/Script/RingDemoController.cs
+++ Assets/Script/RingDemoController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using SimpleJSON;
 public class RingDemoController : CommonController
 {
-    public Transform boneTransform;
-    public GameObject RCube;
-    public GameObject LCube;
+
+    public Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +17,14 @@ public class RingDemoController : CommonController
     // Update is called once per frame
     void Update()
     {
-                //Debug.Log("Hello: " + mesh.mesh.bounds.size.x);
+        //Debug.Log("Hello: " + mesh.mesh.bounds.size.x);
         if (Input.GetKeyDown(KeyCode.UpArrow)){
-           // float dis= sphere.transform.localScale.z*0.001f;
-            boneTransform.localScale = new Vector3(boneTransform.localScale.x, boneTransform.localScale.y+0.1f, boneTransform.localScale.z-0.06f);
-            RCube.transform.position= new Vector3(RCube.transform.position.x+0.1f, RCube.transform.position.y, RCube.transform.position.z);
-            LCube.transform.position= new Vector3(LCube.transform.position.x-0.1f, LCube.transform.position.y, LCube.transform.position.z);
+                  anim.speed = 0.1f;
+                  anim.Play("Inbound");
         }
         if (Input.GetKeyDown(KeyCode.DownArrow)){
-           // float dis= sphere.transform.localScale.z*0.001f;
-            boneTransform.localScale = new Vector3(boneTransform.localScale.x, boneTransform.localScale.y-0.1f, boneTransform.localScale.z+0.06f);
-            RCube.transform.position= new Vector3(RCube.transform.position.x-0.1f, RCube.transform.position.y, RCube.transform.position.z);
-            LCube.transform.position= new Vector3(LCube.transform.position.x+0.1f, LCube.transform.position.y, LCube.transform.position.z);
+                   anim.speed = 1.0f;
+                  anim.Play("InboundOut");
         }
     }
 
@@ -36,5 +32,21 @@ public class RingDemoController : CommonController
         JavaMessage(_data);
         var N = JSON.Parse(_data);
         callLibFunction("printLogForUnity","get data " + _data);
+        if(N["ringData"] != null){
+            var ringData = N["ringData"].Value;  
+            if(ringData=="outBoundIn"){
+                  anim.speed = 0.1f;
+                  anim.Play("OutboundIn");
+            }else if(ringData=="InboundOut"){
+                  anim.speed = 0.1f;
+                  anim.Play("InboundOut");
+            }else if(ringData=="Outbound"){
+                  anim.speed = 0.1f;
+                  anim.Play("Outbound");
+            }else if(ringData=="Inbound"){
+                  anim.speed = 0.1f;
+                  anim.Play("Inbound");
+            }
+        }
     }
 }
